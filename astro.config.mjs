11257762f6b02d72b5cd83791a6f8c0eefad8104
Anna Zezulka <anna.zezulka@proton.me>
2026-02-08 22:26:56 +0100

    Aktualizovaný design, nutno změnit obsah

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   // React zůstává v integracích
-  integrations: [react()],
+  integrations: [react(), icon()],
   
   // Tailwind 4 se dává sem, do Vite pluginů
   vite: {
