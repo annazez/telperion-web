f6ff4ad66a960a0f89aa3b19043aca6865e58872
Anna Zezulka <anna.zezulka@proton.me>
2026-03-08 16:42:07 +0100

    Updated form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
-import react from "@astrojs/react"; // <-- Add this import
+import react from "@astrojs/react"; 
 
 export default defineConfig({
-  // Add react() to the integrations array
   integrations: [icon({ include: { mdi: ["*"] } }), react()],
   vite: {
     plugins: [tailwindcss()],
